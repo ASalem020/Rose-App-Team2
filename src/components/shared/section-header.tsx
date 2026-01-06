50235cd727c1776a5e22d03300cebb295b1493b7
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-01-06 20:02:04 +0200

    feat(testimonials): separate section-header to two components (title - description)

--- src/components/shared/section-header.tsx
+++ src/components/shared/section-header.tsx
@@ -1,4 +1,5 @@
-import { cn } from '@/lib/utils/tailwind-merge';
+import SectionHeaderDescription from './section-header-description';
+import SectionHeaderTitle from './section-header-title';
 
 type SectionHeaderPropsType = {
   title: string;
@@ -12,23 +13,10 @@ export default function SectionHeader({
   return (
     <div className="section-header mb-10 flex flex-col px-5">
       {/* Title */}
-      <h6 className="mb-2 text-start text-sm font-bold uppercase tracking-[.25rem] text-softPink-500 md:text-center md:text-base">
-        {title}
-      </h6>
+      <SectionHeaderTitle title={title} />
 
       {/* Description */}
-      <p
-        className={cn(
-          // Main Styles
-          'relative w-fit text-start text-2xl font-bold text-maroon-700 md:mx-auto md:text-center md:text-3xl lg:text-4xl',
-          // Before Styles
-          'before:absolute before:left-0 before:top-3/4 before:z-0 before:h-4 before:w-[25.125rem] before:rounded-r-2xl before:bg-softPink-100',
-          // After Styles
-          'after:absolute after:-bottom-1 after:left-0 after:z-0 after:h-[0.125rem] after:w-40 after:bg-softPink-600',
-        )}
-      >
-        <span className="relative z-10">{description}</span>
-      </p>
+      <SectionHeaderDescription description={description} />
     </div>
   );
 }
