6ded03b7a2b4bd80cac0b00f656ada3d8975d1e9
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-01-06 13:54:14 +0200

    style(testimonials): make section-header more readablity by using cn function

--- src/components/shared/section-header.tsx
+++ src/components/shared/section-header.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/lib/utils/tailwind-merge';
+
 type SectionHeaderPropsType = {
   title: string;
   description: string;
@@ -15,7 +17,16 @@ export default function SectionHeader({
       </h6>
 
       {/* Description */}
-      <p className="relative w-fit text-start text-2xl font-bold text-maroon-700 before:absolute before:left-0 before:top-3/4 before:z-0 before:h-4 before:w-3/4 before:rounded-r-2xl before:bg-softPink-100 after:absolute after:-bottom-1 after:left-0 after:z-0 after:h-[2px] after:w-1/3 after:bg-softPink-600 md:text-center md:text-3xl lg:text-4xl">
+      <p
+        className={cn(
+          // Main Styles
+          'relative w-fit text-start text-2xl font-bold text-maroon-700 md:text-center md:text-3xl lg:text-4xl',
+          // Before Styles
+          'before:absolute before:left-0 before:top-3/4 before:z-0 before:h-4 before:w-[25.125rem] before:rounded-r-2xl before:bg-softPink-100',
+          // After Styles
+          'after:absolute after:-bottom-1 after:left-0 after:z-0 after:h-[0.125rem] after:w-40 after:bg-softPink-600',
+        )}
+      >
         <span className="relative z-10">{description}</span>
       </p>
     </div>
