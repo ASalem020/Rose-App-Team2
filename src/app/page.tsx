95d8ae9c3e87e5f0276f9964532d6e29a0cff474
Michael Samy <michaelsamy196@gmail.com>
2026-01-09 13:30:05 +0200

    feat(home): add about and companies component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import About from '@/components/features/about';
+import Companies from '@/components/features/companies';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <About />
+      <Companies />
     </div>
   );
 }
