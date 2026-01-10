a8d1b204e580d492f4aa2f53111847409464daae
Michael Samy <michaelsamy196@gmail.com>
2026-01-10 13:36:06 +0200

    feat(gallery): add gallery section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from '@/components/features/about';
 import Companies from '@/components/features/companies';
+import Gallery from '@/components/features/gallery';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <About />
+      <Gallery />
       <Companies />
     </div>
   );
--- src/components/features/gallery.tsx
+++ src/components/features/gallery.tsx
@@ -1,98 +1,87 @@
+import { cn } from '@/lib/utils/tailwind-merge';
+import Image from 'next/image';
 import React from 'react';
 
 export default function Gallery() {
+  const col1 = [
+    {
+      src: '/assets/images/gallery/Frame 74.svg',
+      height: 617,
+    },
+    {
+      src: '/assets/images/gallery/Frame 79.svg',
+      height: 406,
+    },
+  ];
+
+  const col2 = [
+    {
+      src: '/assets/images/gallery/Frame 75.svg',
+      height: 411,
+    },
+
+    {
+      src: '/assets/images/gallery/Frame 78.svg',
+      height: 611,
+    },
+  ];
+
+  const col3 = [
+    {
+      src: '/assets/images/gallery/Frame 76.svg',
+      height: 411,
+    },
+
+    {
+      src: '/assets/images/gallery/Frame 80.svg',
+      height: 611,
+    },
+  ];
+
+  const columns = [col1, col2, col3];
+
   return (
-    <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-      <div className="grid gap-4">
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="rounded-base h-auto max-w-full"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
-            alt=""
-          />
+    <div className="mt-32">
+      {/* Title */}
+      <h6 className="mb-2 text-start text-sm font-bold uppercase tracking-[.25rem] text-softPink-500 md:text-center md:text-base">
+        Gallery
+      </h6>
+
+      {/* Description */}
+      <p
+        className={cn(
+          // Main Styles
+          'relative w-fit text-start text-2xl font-bold text-maroon-700 md:mx-auto md:text-center md:text-3xl lg:text-4xl',
+          // Before Styles
+          'before:absolute before:left-0 before:top-3/4 before:z-0 before:h-4 before:w-3/4 before:rounded-r-2xl before:bg-softPink-100',
+          // After Styles
+          'after:absolute after:-bottom-[0.375rem] after:left-0 after:z-0 after:h-[0.125rem] after:w-40 after:bg-softPink-600',
+          // Dark Styles
+          'dark:text-softPink-200 before:dark:bg-zinc-700 after:dark:bg-softPink-500',
+        )}
+      >
+        <span className="relative z-10">
+          Check Out our Wonderful Gallery
+        </span>
+      </p>
+
+      {/* Gallery */}
+      <div className="mt-10 flex justify-center">
+        <div className="inline-grid grid-flow-col gap-3">
+          {columns.map((column, i) => (
+            <div key={i} className="grid gap-3">
+              {column.map(img => (
+                <Image
+                  key={img.src}
+                  className="rounded-base"
+                  src={img.src}
+                  alt="gallery-image"
+                  width={420}
+                  height={img.height}
+                />
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
