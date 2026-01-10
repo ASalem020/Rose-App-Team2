a71ffd5bba4f49f9c0ea6e54a0564c22e6004c68
Michael Samy <michaelsamy196@gmail.com>
2026-01-10 13:37:41 +0200

    chore(companies): add comments

--- src/components/features/companies.tsx
+++ src/components/features/companies.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 
 export default function Companies() {
+  // Companies Logos
   const companies = [
     'image 36.svg',
     'image 40.svg',
@@ -12,12 +13,14 @@ export default function Companies() {
   ];
   return (
     <div className="m-auto mb-96 mt-36 flex w-fit flex-col gap-10 rounded-2xl bg-maroon-50 px-6 py-10">
+      {/* Heading */}
       <h3 className="text-center text-4xl font-bold text-maroon-700">
         Trusted by over{' '}
         <span className="text-softPink-500">4.5k+</span>{' '}
         companies
       </h3>
 
+      {/* Companies Logos */}
       <div className="flex justify-center gap-16">
         {companies.map((company, index) => (
           <Image
