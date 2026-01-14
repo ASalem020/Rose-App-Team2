db1b3431a9605f08df314d3105882b45fea50908
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-01-14 21:12:56 +0200

    refactor(testimonials): use tailwind classes instead of static values

--- src/app/_components/testimonials/components/testimonial-item.tsx
+++ src/app/_components/testimonials/components/testimonial-item.tsx
@@ -19,9 +19,9 @@ export default function TestimonialItem({
   date,
 }: TestimonialItemPropsType) {
   return (
-    <div className="comment-box relative min-h-60 min-w-[21.4375rem] max-w-[25.25rem] rounded-3xl bg-white p-5">
+    <div className="comment-box relative min-h-60 min-w-80 max-w-[25.25rem] rounded-3xl bg-white p-5">
       {/* Avatar */}
-      <div className="image relative mx-auto -mt-20 size-24 overflow-hidden rounded-full border-4 border-white md:size-[7.5rem]">
+      <div className="image relative mx-auto -mt-20 size-24 overflow-hidden rounded-full border-4 border-white md:size-28">
         <Image
           src={imgSrc}
           alt={`${name} profile photo`}
