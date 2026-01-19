82df51fcee3379611a96a2d03de8b8b225ff46a4
Michael Samy <michaelsamy196@gmail.com>
2026-01-19 12:22:51 +0200

    feat(testing): implement forgot password flow component for testing

---
+++ src/app/[locale]/(auth)/forgot-password/_components/forgot-password-flow.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React, { useState } from 'react';
+import EmailStep from './email-step';
+import NewPasswordStep from './new-password-step';
+
+// NOTE => This component is for testing the flow only
+export default function ForgotPasswordFlow() {
+  const [email, setEmail] = useState<string>('');
+
+  return (
+    // NOTE => waiting for Forgot password Flow and layout to be completed...
+    <div>
+      {/* NOTE
+      For testing the components:
+      1. You can type your registered email , then go to your email and find the OTP
+      2. Enter OTP in postman using verify reset API (until the OTP step finished...)
+      3. then return to that page to type the new password */}
+
+      <EmailStep setEmail={setEmail} />
+      <NewPasswordStep email={email} />
+    </div>
+  );
+}
