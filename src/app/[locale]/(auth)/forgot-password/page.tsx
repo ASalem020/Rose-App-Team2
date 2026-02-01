2f2a8851c35e22014070807743219f2980165345
Michael Samy <michaelsamy196@gmail.com>
2026-02-01 23:39:49 +0200

    fix(style): forgot-flow width fix

--- src/app/[locale]/(auth)/forgot-password/page.tsx
+++ src/app/[locale]/(auth)/forgot-password/page.tsx
@@ -3,7 +3,7 @@ import ForgotPasswordFlow from './_components/forgot-password-flow';
 
 export default function ForgotPasswordPage() {
   return (
-    <div className="w-1/2">
+    <div>
       <ForgotPasswordFlow />
     </div>
   );
