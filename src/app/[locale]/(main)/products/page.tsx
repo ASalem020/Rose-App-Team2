bd8bf375ffcb71f692a4edaa0c9a9fb69bfbe43b
Yassa <yassi@example.com>
2026-02-08 00:31:44 +0200

    refactor(ProductPage): remove unnecessary whitespace in page.tsx

--- src/app/[locale]/(main)/products/page.tsx
+++ src/app/[locale]/(main)/products/page.tsx
@@ -3,10 +3,7 @@ import CategoryFilter from "./_components/filter/category-filter";
 import RatingFilter from "./_components/filter/rating-filter";
 import ResetAllFilters from "./_components/filter/reset-all-filter";
 
-
-
 export default function ProductPage({ searchParams }: BuildSearchparamsProps) {
-
   return (
     <div className="mx-auto container mt-12 grid grid-cols-4 gap-6 ">
       <div className="col-span-1 bg-zinc-50 space-y-6 divide-y-2 *:py-2 p-2 border-1">
