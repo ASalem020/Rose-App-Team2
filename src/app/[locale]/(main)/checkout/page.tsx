776ad4724ebc6ef5c04548fcbb2e993a17cf757e
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-02-11 15:11:25 +0200

    feat(checkout): create reuseable steps-progress-component

--- src/app/[locale]/(main)/checkout/page.tsx
+++ src/app/[locale]/(main)/checkout/page.tsx
@@ -1,3 +1,12 @@
+import StepsProgress from './_components/steps-progress';
+
 export default function Page() {
-  return <div>checkout</div>;
+  return (
+    <div className="checkout-page">
+      <div className="container mx-auto mt-16 px-5">
+        {/* Steps Progress */}
+        <StepsProgress currentStep={1} />
+      </div>
+    </div>
+  );
 }
