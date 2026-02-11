9b0201bda8fd7442bd2b6c1dc957cdc163b25a90
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-02-11 16:39:36 +0200

    feat(checkout): create address card component

---
+++ src/app/[locale]/(main)/checkout/_components/address-card.tsx
@@ -0,0 +1,70 @@
+import { cn } from '@/lib/utils/tailwind-merge';
+import { Phone } from 'lucide-react';
+
+type AddressCardProps = {
+  city: string;
+  address: string;
+  phone: string;
+  selected?: boolean;
+};
+
+export default function AddressCard({
+  city,
+  address,
+  phone,
+  selected,
+}: AddressCardProps) {
+  return (
+    <div
+      data-selected={selected}
+      className="address-card group: cursor-pointer rounded-xl border border-zinc-300 px-4 py-3.5 duration-300 hover:bg-zinc-50 data-[selected=true]:bg-maroon-600 data-[selected=true]:text-white"
+    >
+      {/* Header */}
+      <div className="header flex items-center justify-between">
+        {/* City */}
+        <h4
+          className={cn(
+            'text-xl font-semibold md:text-2xl',
+            selected ? 'text-white' : 'text-zinc-800',
+          )}
+        >
+          {city}
+        </h4>
+
+        {/* Phone */}
+        <div className="phone flex items-center gap-2.5">
+          <span
+            className={cn(
+              'icon flex size-8 items-center justify-center rounded-full',
+              selected
+                ? 'bg-white text-maroon-600'
+                : 'bg-maroon-600 text-white',
+            )}
+          >
+            <Phone size={20} />
+          </span>
+          <span
+            className={cn(
+              'number',
+              selected ? 'text-white' : 'text-zinc-500',
+            )}
+          >
+            {phone}
+          </span>
+        </div>
+      </div>
+
+      {/* Address */}
+      <p
+        className={cn(
+          'address mt-1.5 w-fit rounded-full px-3 py-1 text-sm font-medium md:text-base',
+          selected
+            ? 'bg-zinc-800 text-white'
+            : 'bg-zinc-100 text-zinc-800',
+        )}
+      >
+        {address}
+      </p>
+    </div>
+  );
+}
--- src/app/[locale]/(main)/checkout/page.tsx
+++ src/app/[locale]/(main)/checkout/page.tsx
@@ -1,3 +1,4 @@
+import AddressCard from './_components/address-card';
 import StepsProgress from './_components/steps-progress';
 
 export default function Page() {
@@ -6,6 +7,34 @@ export default function Page() {
       <div className="container mx-auto mt-16 px-5">
         {/* Steps Progress */}
         <StepsProgress currentStep={1} />
+
+        {/* Shipping Addresses */}
+        {/* Title */}
+        <h3 className="mb-6 text-2xl font-semibold md:text-3xl">
+          Shipping Addresses
+        </h3>
+
+        {/* Addresses */}
+        <div className="addresses">
+          <div className="container flex max-h-[20.9375rem] flex-col gap-3 overflow-y-auto">
+            <AddressCard
+              city="Cairo"
+              address="14 Omar Ibn Akhatab St., Ramsis St., Cairo"
+              phone="123-456-7890"
+            />
+            <AddressCard
+              city="Cairo"
+              address="14 Omar Ibn Akhatab St., Ramsis St., Cairo"
+              phone="123-456-7890"
+              selected
+            />
+            <AddressCard
+              city="Cairo"
+              address="14 Omar Ibn Akhatab St., Ramsis St., Cairo"
+              phone="123-456-7890"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
