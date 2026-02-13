f9aa61aa101129e7cd8bd65c497f6f00c5ef3ccc
ASalem020 <ahmedmsalem020@gmail.com>
2026-02-13 05:10:06 +0200

    feat(addresses):completed the UI & logic - Enhanced UX - Added translation - Added Add Address Button to the home page

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -6,10 +6,14 @@ import BestSelling from '@/app/[locale]/(main)/_components/sections/best-selling
 import MostPopular from '@/app/[locale]/(main)/_components/sections/most-popular';
 import OccasionsSection from '@/components/cards/card-List';
 import SupportSection from '@/components/appsupport/support-sections';
+import { AddAddressButton } from '@/components/features/address/add-address-button';
 
 export default function Home() {
   return (
     <div className="bg-white dark:bg-zinc-800">
+      <div className="container mx-auto py-8 flex justify-center">
+        <AddAddressButton />
+      </div>
       <OccasionsSection />
       <SupportSection />
       <BestSelling />
