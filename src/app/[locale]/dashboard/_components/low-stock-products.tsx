e4841b6c78135f937cd780c8edd4dd0e2b7c4b3f
Yassa <yassi@example.com>
2026-02-24 20:18:18 +0200

    feat(top-selling-products): wrap product cards in Suspense for loading state

--- src/app/[locale]/dashboard/_components/low-stock-products.tsx
+++ src/app/[locale]/dashboard/_components/low-stock-products.tsx
@@ -1,6 +1,8 @@
 import TitleComponents from './title';
 import LowStockCard from './low-stock-card';
 import { useTranslations } from 'next-intl';
+import { Suspense } from 'react';
+import CardSkeleton from './skeleton/card-skeleton';
 
 export default function LowStockProducts() {
   // ^ Translations
@@ -10,7 +12,9 @@ export default function LowStockProducts() {
     <div className="flex max-h-112 flex-col rounded-2xl bg-white p-6">
       <TitleComponents title={t('low-stock-products')} />
       <div className="flex-1 overflow-y-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-        <LowStockCard />
+        <Suspense fallback={<CardSkeleton />}>
+          <LowStockCard />
+        </Suspense>
       </div>
     </div>
   );
--- src/app/[locale]/dashboard/_components/top-selling-products.tsx
+++ src/app/[locale]/dashboard/_components/top-selling-products.tsx
@@ -1,6 +1,8 @@
 import { useTranslations } from 'next-intl';
 import TitleComponents from './title';
 import TopSellingCard from './top-selling-card';
+import { Suspense } from 'react';
+import CardSkeleton from './skeleton/card-skeleton';
 
 export default function TopSellingProducts() {
   // ^ Translations
@@ -10,7 +12,9 @@ export default function TopSellingProducts() {
     <div className="flex max-h-112 flex-col rounded-2xl bg-white p-6">
       <TitleComponents title={t('top-selling-products')} />
       <div className="flex-1 overflow-y-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-        <TopSellingCard />
+        <Suspense fallback={<CardSkeleton />}>
+          <TopSellingCard />
+        </Suspense>
       </div>
     </div>
   );
