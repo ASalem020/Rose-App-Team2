7285ea8a5b52b092cbeac9098efd513dc370f6b1
Michael Samy <michaelsamy196@gmail.com>
2026-03-12 13:48:54 +0200

    refactor(product): add in product interface

--- src/lib/types/product.d.ts
+++ src/lib/types/product.d.ts
@@ -10,8 +10,8 @@ export interface Product {
   sold: number;
   rateAvg: number;
   rateCount: number;
-  images: FileList | null;
-  imgCover: File;
+  images: string[];
+  imgCover: string;
   category: string;
   occasion: string;
   isSuperAdmin: boolean;
@@ -49,4 +49,40 @@ export type RecommendedProductsAPIResponse = {
   message: string;
   count: number;
   recommendations: RecommendedProduct[];
+};
+
+export interface Product {
+  _id: string;
+  title: string;
+  description: string;
+  slug: string;
+  price: number;
+  priceAfterDiscount: number;
+  discount: number;
+  quantity: number;
+  sold: number;
+  rateAvg: number;
+  rateCount: number;
+  images: FileList | null;
+  imgCover: File;
+  category: string;
+  occasion: string;
+  isSuperAdmin: boolean;
+  isInWishlist: boolean;
+  favoriteId: string | null;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+export type ProductAPIResponse = {
+  message: string;
+  metadata: {
+    currentPage: number;
+    totalPages: number;
+    limit: number;
+    totalItems: number;
+    nextPage: number;
+  };
+  products: Product[];
 };
